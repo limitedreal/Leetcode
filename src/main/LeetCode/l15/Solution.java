061727998ed22bd9cb6d06fdd77b94258d70952d
tyrealtian <tyrealtian@tencent.com>
2021-07-26 16:32:42 +0800

    7.26

--- src/main/LeetCode/l15/Solution.java
+++ src/main/LeetCode/l15/Solution.java
@@ -1,87 +1,46 @@
 package main.LeetCode.l15;
 
+import java.lang.annotation.Target;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-        //List<Integer> list = new ArrayList<>();
-        //list.add(sc.nextInt());
-        //int n = list.size();
-        //int[] nums = new int [n];
-        //for (int i = 0;i<n;i++){
-        //    nums[i] = list.get(i);
-        //}
-        int[] nums = {0,0,0};
-        int i = 0, j = 1, k = 2;
-        String temp = nums[i] + "," + nums[j] + "," + nums[k];
-        String temp2 = nums[i] + "," + nums[j] + "," + nums[k];
-        Set<String> set = new HashSet<>();
-        set.add(temp);
-        System.out.println(set.contains(temp2));
-        System.out.println(set);
-
+        int[] nums = {0, 0, 0};
         threeSum(nums);
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        Set<String> repetition = new HashSet<>();
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int key : nums) {
-            if(map.containsKey(key)){
-                map.put(key,map.get(key)+1);
-            }else{
-                map.put(key,1);
-            }
-
-        }
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < n - 2; i++) {//最后两个就不用加入遍历了，顺便保证下面的不会越界
-            //剪枝
-            if (nums[i] + nums[n - 1] + nums[n - 2] < 0) {//说明这个数太小了，根本不可能找得到
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            if (nums[i] > 0) {
-                break;
-            }
-
-            for (int j = i + 1; j < n - 1; j++) {
-                //剪枝
-                if (nums[i] + nums[j] + nums[n - 1] < 0) {
-                    continue;
-                }
-                if (nums[i] + nums[j] > 0) {
-                    break;
-                }
-                int k = -(nums[i] + nums[j]);
-                if(map.containsKey(k)){//说明存在这种情况
-                    if(k<nums[i]||k<nums[j]){
-                        continue;
+            int low = i+1;
+            int high = nums.length - 1;
+            while (low < high) {
+                int now = nums[low] + nums[high];
+                int target = -nums[i];
+                if (now == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[low]);
+                    list.add(nums[high]);
+                    while (low < high && nums[low] == nums[low + 1]) {
+                        low++;
                     }
-                    int count = map.get(k);
-                    if(nums[i]==k){
-                        count--;
+                    while (low < high && nums[high] == nums[high - 1]) {
+                        high--;
                     }
-                    if(nums[j]==k){
-                        count--;
-                    }
-                    if(count<=0){
-                        continue;
-                    }
-                    String test = nums[i] + "," + nums[j] + "," + k;
-                    if (repetition.contains(test)) {//如果存在，就可以跳过了
-                        continue;
-                    }
-                    repetition.add(test);
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(k);
-
-                    result.add(temp);
+                    result.add(list);
+                    low++;
+                    high--;
+                } else if (now < target) {
+                    low++;
+                } else {
+                    high--;
                 }
             }
         }
